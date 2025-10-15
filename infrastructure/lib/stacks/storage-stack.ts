a2832802a88d0873c7501c1f58c986bc4c591ead
vberkoz <vberkoz@gmail.com>
2025-10-16 00:55:39 +0300

    Configure Route53 hosted zone with CDK for domain management

--- infrastructure/lib/stacks/storage-stack.ts
+++ infrastructure/lib/stacks/storage-stack.ts
@@ -3,6 +3,9 @@ import * as dynamodb from 'aws-cdk-lib/aws-dynamodb'
 import * as s3 from 'aws-cdk-lib/aws-s3'
 import * as cloudfront from 'aws-cdk-lib/aws-cloudfront'
 import * as origins from 'aws-cdk-lib/aws-cloudfront-origins'
+import * as route53 from 'aws-cdk-lib/aws-route53'
+import * as targets from 'aws-cdk-lib/aws-route53-targets'
+import * as acm from 'aws-cdk-lib/aws-certificatemanager'
 import { Construct } from 'constructs'
 
 export class StorageStack extends cdk.Stack {
@@ -11,6 +14,7 @@ export class StorageStack extends cdk.Stack {
     assets: s3.Bucket
   }
   public readonly distribution: cloudfront.Distribution
+  public readonly hostedZone: route53.IHostedZone
 
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props)
@@ -57,6 +61,19 @@ export class StorageStack extends cdk.Stack {
       })
     }
 
+    const domainName = 'vberkoz.com'
+    const subdomain = 'waitlist'
+    const fullDomain = `${subdomain}.${domainName}`
+
+    this.hostedZone = route53.HostedZone.fromLookup(this, 'HostedZone', {
+      domainName: domainName
+    })
+
+    const certificate = new acm.Certificate(this, 'Certificate', {
+      domainName: fullDomain,
+      validation: acm.CertificateValidation.fromDns(this.hostedZone)
+    })
+
     this.distribution = new cloudfront.Distribution(this, 'AssetsDistribution', {
       defaultBehavior: {
         origin: new origins.S3Origin(this.buckets.assets),
@@ -66,8 +83,16 @@ export class StorageStack extends cdk.Stack {
         compress: true,
         cachePolicy: cloudfront.CachePolicy.CACHING_OPTIMIZED
       },
+      domainNames: [fullDomain],
+      certificate: certificate,
       priceClass: cloudfront.PriceClass.PRICE_CLASS_100,
       comment: 'Waitlist Assets CDN'
+    })
+
+    new route53.ARecord(this, 'AliasRecord', {
+      zone: this.hostedZone,
+      recordName: subdomain,
+      target: route53.RecordTarget.fromAlias(new targets.CloudFrontTarget(this.distribution))
     })
 
     new cdk.CfnOutput(this, 'AssetsBucketName', {
@@ -88,6 +113,11 @@ export class StorageStack extends cdk.Stack {
     new cdk.CfnOutput(this, 'CloudFrontDomainName', {
       value: this.distribution.distributionDomainName,
       description: 'CloudFront distribution domain name'
+    })
+
+    new cdk.CfnOutput(this, 'CustomDomainName', {
+      value: fullDomain,
+      description: 'Custom domain name'
     })
   }
 }
